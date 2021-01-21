6fd5b0ae45523613bca922a91ccc7590b8ccc5f8
SWIZCLOUD <65864777+SwizCloudFR@users.noreply.github.com>
2021-01-21 17:21:34 +0100

    Create Alert.php

---
+++ Alert.php
@@ -0,0 +1,28 @@
+class Error {
+
+	public static function Success($error)
+	{
+		return "<div class='alert alert-success'><b>Bravo !</b> {$error}</div>";
+	}
+
+	public static function Warning($error)
+	{
+		return "<div class='alert alert-warning'><b>Attention !</b> {$error}</div>";
+	}
+
+	public static function Danger($error)
+	{
+		return "<div class='alert alert-danger'><b>Erreur !</b> {$error}</div>";
+	}
+
+	public static function Info($error)
+	{
+		return "<div class='alert alert-info'><b>Information !</b> {$error}</div>";
+	}
+
+	public static function Primary($error)
+	{
+		return "<div class='alert alert-danger'>{$error}</div>";
+	}
+
+}
